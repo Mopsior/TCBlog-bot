f15ca307d3b1982418c084e2325990491a72ed11
Mopsior <80514290+Mopsior@users.noreply.github.com>
2021-03-12 20:43:20 +0100

    Add files via upload

---
+++ 12.03 - Embedy, komenda embed/index.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const fs = require('fs')
+
+const prefix = '!'
+const token = 'ODE4OTAxNjIxMDcyMzMwNzgy.YEezkA.BtcAWV_Qgef4krMSfKqEcYXtF98'
+
+client.once('ready', () => {
+    console.log('Bot jest gotowy!')
+})
+
+client.commands = new Discord.Collection()
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`)
+
+    client.commands.set(command.name, command)
+}
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return
+
+    const args = message.content.slice(prefix.length).toLowerCase().split(/ +/);
+    const command = args.shift().toLowerCase()
+
+    if (command === 'ping') {
+        client.commands.get('ping').execute(message, args)
+    }
+    else if (command === 'say') {
+        client.commands.get('say').execute(message, args)
+    }
+    else if (command === 'embed') {
+        client.commands.get('embed').execute(message, args, Discord)
+    }
+})
+
+
+client.login(token)
+
+client.on('debug', () => {})
+client.on('warn', msg => {
+    client.log(info)
+})
+client.on('error', () => {})
